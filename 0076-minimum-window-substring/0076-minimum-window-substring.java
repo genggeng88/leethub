46cf4387fa5150543c6177ffc2d51fb42f27d8b4
genggeng88 <59033589+genggeng88@users.noreply.github.com>
2026-06-27 09:37:57 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        
+    }
+}
+
+
+s = hnlsihneh
+    i
+    j
+t = hsi           => map<char, cnt> => cntNeeded = map.size()
