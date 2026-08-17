a140ed2e397af8cd789fdaa868b060c4787f4f31
genggeng88 <59033589+genggeng88@users.noreply.github.com>
2026-08-17 13:06:52 -0700

    Time: 10 ms (80.34%), Space: 49 MB (51.04%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,46 +1,45 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int res = 0;
-        List<List<int[]>> adjs = new ArrayList<>();
+        List<List<int[]>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            adjs.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
+
         for (int[] time : times) {
-            int from = time[0], to = time[1], weight = time[2];
-            adjs.get(from - 1).add(new int[]{to, weight});
+            int start = time[0], end = time[1], weight = time[2];
+            graph.get(start - 1).add(new int[]{end, weight});
         }
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[1], b[1]);
+            }
+        });
 
         int[] delayTimes = new int[n];
         Arrays.fill(delayTimes, Integer.MAX_VALUE);
         delayTimes[k-1] = 0;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>(){
-            @Override
-            public int compare(int[] a, int[] b) {
-                return Integer.compare(a[1], b[1]); 
-            }
-        });
-
         minHeap.offer(new int[]{k, 0});
 
         while (!minHeap.isEmpty()) {
             int[] node = minHeap.poll();
-            int dist = node[1];
-            if (dist > delayTimes[node[0] - 1]) continue;
+            int start = node[0], weight = node[1];
+            if (weight > delayTimes[start - 1]) continue;
 
-            delayTimes[node[0] - 1] = node[1];
-            List<int[]> neighbors = adjs.get(node[0] - 1);
-            for (int[] neighbor : neighbors) {
-                int newDist = dist + neighbor[1];
-                if (newDist < delayTimes[neighbor[0] - 1]) {
-                    delayTimes[neighbor[0] - 1] = newDist;
-                    minHeap.offer(new int[]{neighbor[0], newDist});
+            for (int[] nei : graph.get(start - 1)) {
+                int news = nei[0], neww = weight + nei[1];
+                if (neww < delayTimes[news - 1]) {
+                    delayTimes[news - 1] = neww;
+                    minHeap.offer(new int[]{news, neww});
                 }
             }
         }
 
-        for (int delayTime : delayTimes) {
-            if (delayTime == Integer.MAX_VALUE) return -1;
-            res = Math.max(res, delayTime);
+        int res = 0;
+        for (int num : delayTimes) {
+            if (num == Integer.MAX_VALUE) return -1;
+            res = Math.max(res, num);
         }
         return res;
     }
